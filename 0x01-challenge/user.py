3e905c84b5fdee611bba8c3c67d168e3858fd620
Ibrahim Uzairu <ibrahimuzairu28@gmail.com>
2023-12-16 13:01:54 +0000

    Task2

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
-""" 
+"""
 User class
 """
 
 class User():
-    """ Documentation """
+    """ Defines a user. """
 
     def __init__(self):
-        """ Documentation """
+        """ Initializes instance. """
         self.__email = None
-
-    @email.setter
-    def email(self, value):
-        """ Documentation """
-        if type(value) is not str:
-            raise TypeError("email must be a string")
-        self.__email = value
 
     @property
     def email(self):
-        """ Documentation """
+        """ Gets the email. """
         return self.__email
-   
-    
-if __name__ == "__main__":
 
+    @email.setter
+    def email(self, value):
+        """ Sets the email. """
+        if not isinstance(value, str):
+            raise TypeError("email must be a string")
+        self.__email = value
+
+
+if __name__ == "__main__":
     u = User()
     u.email = "john@snow.com"
     print(u.email)
+                                                                                                                        
